e42c3556cbbf59f7361f26147472d40cd7dca6bd
seoeuncho <westsilver24@naver.com>
2023-07-26 22:10:24 +0900

    union type, any, unknown, 함수에 타입지정하는 법, void 타입

--- index.ts
+++ index.ts
@@ -1,18 +1,56 @@
 // type : string, number, boolean, null, undefined, bigint,, [], {} 등
 
-//1. 기본 타입스크립트 문법
+//1. 변수 타입지정 가능
 let lastName: string = "kim";
 lastName = "park";
 lastName = "123";
 
+// 타입지정 문법 생략가능 - 자동으로 지정됨
+let test = "kim";
+
+// 변수만 만들고 할당을 해주면 자동으로 타입 지정됨
+let test2;
+test = "cho";
+
+// any 타입 : 타입실드 해제문법 = 일반 JS 변수
+let test3: any;
+test3 = 123;
+test3 = [];
+
+// unknown 타입 : 모든 자료형 허용, 타입 실드가 해제된 것은 아니라 any보다 안전함
+let test4: unknown;
+test4 = 123;
+test4 = {};
+// let test5 :string = test3; // test3가 any 타입이라서 타입실드가 죽은 상태라 가능해짐, 디버깅 어려움
+// let test5 :string = test4; // error
+
+let user: string = "kim";
+let age: undefined | number = undefined;
+let married: boolean = false;
+let info: (string | number | undefined | boolean)[] = [user, age, married];
+
+let school: {
+  score: (number | boolean)[];
+  teacher: string;
+  friend: string | string[];
+} = {
+  score: [100, 97, 84],
+  teacher: "Phil",
+  friend: "John",
+};
+school.score[4] = false;
+school.friend = ["Lee", school.teacher];
+
 //2. Array
 let nameArr: string[] = ["kim", "park"];
 nameArr = ["cho", "123"];
+let testArr: (string | number)[] = ["kim", "1", 2];
+let testArr2: { a: string | number } = { a: 123 };
 
 // 3. Object
-let nameObj: { name: string } = { name: "kim" };
+let nameObj: { name: string; age: number } = { name: "kim", age: 27 };
 // let nameObj: { name?: string } = {};
-nameObj = { name: "cho" };
+nameObj = { name: "cho", age: 28 };
 
 // 4. Union Type (= or)
 let testName1: string[] | number = 123;
@@ -23,11 +61,51 @@ type nameType = string | number;
 let testName2: nameType = "kim";
 testName2 = 123;
 
-// 5. 함수 : 파라미터, return 값 타입지정 가능
+// 5. 함수 : 파라미터, return 타입지정 가능
 function solution(x: number): number {
   return x * 2;
 }
-solution(3);
+solution(3); // 6
+solution(4); // 8
+
+// 파라미터가 옵션일 경우 '?' 문법 (= '?.' 옵셔널체이닝)
+function solution2(x?: number): number {
+  return x * 3;
+}
+solution2();
+
+// 함수에서 void 타입 활용가능 : return이 없을 경우, 실수로 무언가를 return 하는 것을 방지
+function result(x: number): void {
+  1 + 1;
+}
+
+function setName(x?: string): void {
+  if (x) console.log("안녕하세요 " + x + "님.");
+  else console.log("이름이 없습니다.");
+}
+setName(); // "이름이 없습니다."
+setName("홍길동"); // "안녕하세요 홍길동님."
+
+function checkLen(x: string | number): number {
+  return x.toString().length;
+}
+checkLen("123");
+checkLen(123);
+
+function canMarriage(
+  income: number,
+  house: boolean,
+  grade: string
+): string | void {
+  let score: number = 0;
+  score += income;
+
+  if (house) score += 500;
+  if (grade === "상") score += 100;
+  if (score >= 600) return "결혼가능";
+}
+console.log(canMarriage(700, false, '중')); // 결혼가능
+console.log(canMarriage(100, false, '상'));
 
 // 6. Array에 쓸 수 있는 tuple 타입
 type Member = [number, boolean];
